32d4a973fb3533ab1dea1aad6af4943fa99f04ee
Panankosi <parna.pm25@gmail.com>
2023-04-20 11:59:56 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - print a line of code using puts
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
