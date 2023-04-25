fb98a942549c4581d8d24a72b2f2c45bb428c0f9
Panankosi <parna.pm25@gmail.com>
2023-04-25 11:07:35 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,3 +23,12 @@ void times_table(void)
 				_putchar(prod / 10 + 48);
 				_putchar(prod % 10 + 48);
 			}
+			else
+			{
+				_putchar(' ');
+				_putchar(prod + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
